208eb7e9dba3bc116059623e945a8d23fba013c8
radleigh123 <103168649+radleigh123@users.noreply.github.com>
2024-04-15 22:49:02 +0800

    Added additional functionalities to the RPS

--- script.js
+++ script.js
@@ -14,35 +14,65 @@ var getComputerChoice = () => {
 };
 
 var score = 0;
+var limit = 0;
 var playRound = (playerSelection, computerSelection) => {
-    if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    if (playerSelection == "rock" && computerSelection == "scissors") {
         score++;
+        limit++;
         return "You Win! Rock beats Scissors";
     }
-    else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    else if (playerSelection == "paper" && computerSelection == "rock") {
         score++;
+        limit++;
         return "You Win! Paper beats Rock";
     }
-    else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
         score++;
+        limit++;
         return "You Win! Scissors beats Paper";
     }
     else if (playerSelection == computerSelection)
         return "It's a Draw";
-    else
+    else {
+        limit++;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
 };
 
-var playerInput;
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playerInput = prompt("Enter your weapon");
+function playGame(playerInput) {
+    if (limit < 5) {
         let res = playRound(playerInput.toLowerCase(), getComputerChoice().toLowerCase());
 
-        if (res == "It's a Draw") i--;
-    }
+        div.textContent = res;
+        p.textContent = score;
 
-    console.log(score >= 3 ? "You're the winner" : "You Lost");
+        body.appendChild(div);
+        body.appendChild(p);
+    } else {
+        if (score >= 3) {
+            alert("You won");
+        } else {
+            alert("Game over");
+        }
+    }
 }
 
-playGame();
+const rockBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+
+rockBtn.addEventListener("click", () => playGame(rockBtn.textContent));
+paperBtn.addEventListener("click", () => playGame(paperBtn.textContent));
+scissorsBtn.addEventListener("click", () => playGame(scissorsBtn.textContent));
+
+const body = document.querySelector("body");
+body.appendChild(rockBtn);
+body.appendChild(paperBtn);
+body.appendChild(scissorsBtn);
+
+rockBtn.textContent = "Rock";
+paperBtn.textContent = "Paper";
+scissorsBtn.textContent = "Scissors";
+
+const div = document.createElement("div");
+const p = document.createElement("p");
